f5df58c377a22600782eaf1c4f6c3b44d0fbbb10
Mada <git@bitbucket.org:mgiurgiu>
2018-04-19 11:02:11 +0200

    first basic website

---
+++ www/workflow/js/navigateScript.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+
+    loadPage();
+
+    $('a[href="#"]').click(function () {
+        window.history.pushState('', null, '/#home');
+        loadHome();
+    });
+
+    $('a[href="#home"]').click(function () {
+        window.history.pushState('', null, '/#home');
+        loadHome();
+    });
+
+    $('a[href="#project"]').click(function () {
+        window.history.pushState('', null, '/#project');
+        loadProject();
+    });
+
+    function loadPage() {
+        switch (window.location.hash) {
+            case "#project":
+                loadProject();
+            default:
+                loadHome();
+        }
+    }
+
+    function loadProject() {
+        $('#page-content').load("project.html #page-content > *", function (responseText, textStatus, XMLHttpRequest) {
+
+        });
+    }
+
+    function loadHome() {
+        window.history.pushState('', null, '/#home');
+        $('#page-content').load("home.html #page-content > *", function (responseText, textStatus, XMLHttpRequest) {
+            jQuery.getScript("js/homeScript.js")
+                .done(function () {
+                    //console.log("done load homeScript.js");
+                })
+                .fail(function () {
+                    //console.log("fail load homeScript.js");
+                });
+
+            $('#workflow').load("workflow/index.html > *", function (responseText, textStatus, XMLHttpRequest) {
+            });
+        });
+    }
+});
+
