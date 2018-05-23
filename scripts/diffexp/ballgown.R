566df7ec562d3f2eaa6706483d0ce07d48a85b00
Diana Iacob <diana_iacob11@yahoo.com>
2018-05-24 00:13:58 +0200

    plots

--- scripts/diffexp/ballgown.R
+++ scripts/diffexp/ballgown.R
@@ -4,10 +4,12 @@ library(gplots)
 library(genefilter)
 library(GenomicRanges)
 library(plyr)
+set.seed(1234)
 
 # read samples into R
 data_directory = ('/Users/Diana/Desktop/greenteam/dataBallgown')
 sample_IDs = list.dirs(path = data_directory, full.names = FALSE, recursive = FALSE)
+sample_IDs <- sample_IDs[ grepl("^Mlet7", sample_IDs) ]
 sample_paths = paste(data_directory, sample_IDs, sep="/")
 bg = ballgown(samples=sample_paths, meas='all')
 
@@ -20,9 +22,7 @@ structure(bg)$trans
 
 plotTranscripts('NONMMUG034479.2', bg, samples=sample_IDs, meas='FPKM', colorby='transcript')
 
-grep("a", readLines("myfile.dat"), value = TRUE)
-
-pData(bg) = data.frame(id=sampleNames(bg), group=rep(1:0, c(4,4))) # first 4 samples = 1, last 4 samples = 0
+pData(bg) = data.frame(id=sampleNames(bg), group=rep(1:0, c(7,7))) # first 4 samples = 1, last 4 samples = 0
 phenotype_table = pData(bg)
 stat_results = stattest(bg, feature='transcript', meas='FPKM', covariate='group', getFC=TRUE)
 head(stat_results)
@@ -31,7 +31,7 @@ head(stat_results)
 bg_filtered = subset(bg,"rowVars(texpr(bg)) >1",genomesubset=TRUE)
 
 # Identify transcripts that show statistically significant differences between groups.
-results_transcripts = stattest(bg_filtered,feature="transcript", covariate="group", libadjust =TRUE, getFC=TRUE, meas="FPKM")
+results_transcripts = stattest(bg_filtered,feature="transcript", covariate="group", getFC=TRUE, meas="FPKM")
 
 # Identify genes that show statistically significant differences between groups
 results_genes = stattest(bg_filtered, feature="gene", covariate="group", getFC=TRUE, meas="FPKM")
@@ -45,11 +45,9 @@ results_transcripts = data.frame(geneNames=ballgown::geneNames(bg_filtered),
 results_transcripts = arrange(results_transcripts, pval)
 results_genes = arrange(results_genes, pval)
 
-head(results_genes, 10)
-head(results_transcripts, 10)
+head(results_genes, 20)
+head(results_transcripts, 20)
 
-
-results_transcripts
 tropical= c('darkorange', 'dodgerblue','hotpink', 'limegreen', 'yellow')
 palette(tropical)
 
@@ -78,7 +76,7 @@ abline(v=2, col="black", lwd=2, lty=2)
 hist(results_genes[sig,"de"], breaks=50,  main="Distribution of differential expression values", col=as.numeric(phenotype_table$group), las=2)
 
 # Plot the structure and expression levels in a sample of all transcripts that share the same gene locus.
-plotMeans('NONMMUG038229.2', bg_filtered, groupvar="group",legend=TRUE)
+plotMeans('ENSMUSG00000035696.15', bg_filtered, groupvar="group",legend=TRUE)
 
 plotTranscripts(ballgown::geneIDs(bg_filtered)[1729], bg_filtered, main=c('Gene XIST in sample ERR188234'), sample=c('ERR188234'))
 
