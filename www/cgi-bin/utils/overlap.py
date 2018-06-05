c4c9ea4abaf7042445b61d68eab9d02fd45975cd
Mada <git@bitbucket.org:mgiurgiu>
2018-06-05 09:20:58 +0200

    overlap tree interval

--- www/cgi-bin/utils/overlap.py
+++ www/cgi-bin/utils/overlap.py
@@ -4,6 +4,8 @@ __author__ = 'Mada'
 
 ########## find overlaps between genes
 import json
+import intervaltree
+import re
 
 def getOverlap(a, b):
     return max(0, min(a[1], b[1]) - max(a[0], b[0]))
@@ -52,6 +54,55 @@ def findOverlaps(file1, file2=None):
         except FileNotFoundError:
             print("your file does not exist")
 
+def findOverlaps_lnc2gene(file):
+
+    # construct interval tree
+    it_regions  = intervaltree.IntervalTree()
+    try:
+        with open(file, "r") as f:
+            for line in f:
+                if line.startswith("#"):
+                    continue
+                arr = line.strip().replace("\n","").split("\t")
+                chr=arr[0]
+                start=int(arr[3])
+                stop=int(arr[4])
+                type=arr[2]
+                geneID=re.match("gene_id \"(\w+)\";", arr[8], flags=0)
+                transcriptID=re.match("transcript_id \"(\w+)\";", arr[8], flags=0)
+                exonnumber=re.match("exon number (\d);",arr[8],flags=0)
+
+                if stop > start:
+                    dict = {}
+                    dict['chr'] = chr
+                    dict['type'] = type
+                    if geneID is not None:
+                        dict['geneUD'] = geneID
+                    if transcriptID is not None:
+                        dict['transcriptID'] = transcriptID
+                    if exonnumber is not None:
+                        dict['exonnumber'] = exonnumber
+
+                    it_regions[start:stop] = dict
+                    # print(it_regions[start:stop])
+                   # print(it_regions[start:stop])
+
+
+        ##### find overlaps
+
+        for (start,stop,data) in it_regions:
+            # print(p)
+            if data["geneID"].startWith("LNC"):
+                print("do")
+                # fi = find("LNC")
+        # tree.intersect( start, end, lambda x: out.append(x) )
+        # list = [ (x.start, x.end) for x in out ]
+
+
+    except FileNotFoundError:
+        print("your file does not exist")
+
 if __name__ == "__main__":
     # findOverlaps("/home/proj/biocluster/praktikum/neap_ss18/neapss18_noncoding/Noncoding/data/mapping/mm10_primary_assembly_and_lncRNA.json")
-    findOverlaps("mm10_primary_assembly_and_lncRNA.json")
+    # findOverlaps("mm10_primary_assembly_and_lncRNA.json")
+    findOverlaps_lnc2gene("chr18.gtf")
